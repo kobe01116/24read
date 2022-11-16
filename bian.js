fc012fdd51cc06d4a8857ec2f8f6bc5dad38882f
kobe01116 <38522114+kobe01116@users.noreply.github.com>
2022-11-16 15:13:06 +0700

    Update bian.js

--- bian.js
+++ bian.js
@@ -244,7 +244,7 @@ function 開始() {
     id("com.binance.dev:id/2131365994").indexInParent(1).findOne().click()
 
     sleep(500)
-    id("et_tp_trigger_price").findOne().setText("30")
+    id("et_tp_trigger_price").findOne().setText("35")
     sleep(500)
     fun.点击id("iv_sl_input_type")
     sleep(500)
