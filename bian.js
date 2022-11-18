32368543bf1087bebd836a0fd404c7dfe7e65a2d
kobe01116 <38522114+kobe01116@users.noreply.github.com>
2022-11-18 14:52:32 +0700

    Update bian.js

--- bian.js
+++ bian.js
@@ -454,6 +454,122 @@ function 更新() {
 
 }
 
+
+
+threads.start(function () {
+  var window = floaty.window(
+    <frame>
+      <button
+        id="action"
+        text="更新2"
+        textSize="11sp"
+        textColor="yellow"
+        bg="#000000"
+        w="230px"
+        h="98px"
+      ></button>
+
+    </frame>
+  );
+  window.setPosition(device.width - 250, (device.height - 850));
+
+  setInterval(() => { }, 1000);
+
+  var execution = null;
+
+  //记录按键被按下时的触摸坐标
+  var x = 0,
+    y = 0;
+  //记录按键被按下时的悬浮窗位置
+  var windowX, windowY;
+  //记录按键被按下的时间以便判断长按等动作
+  var downTime;
+
+  window.action.setOnTouchListener(function (view, event) {
+    switch (event.getAction()) {
+      case event.ACTION_DOWN:
+        x = event.getRawX();
+        y = event.getRawY();
+        windowX = window.getX();
+        windowY = window.getY();
+        downTime = new Date().getTime();
+        return true;
+      case event.ACTION_MOVE:
+        //移动手指时调整悬浮窗位置
+
+
+        return true;
+      case event.ACTION_UP:
+        //手指弹起时如果偏移很小则判断为点击
+        if (
+          Math.abs(event.getRawY() - y) < 5 &&
+          Math.abs(event.getRawX() - x) < 5
+        ) {
+          //如果按下的时间超过0.2秒判断为长按，退出脚本
+          if (new Date().getTime() - downTime > 200) {
+            window.action.setText("记起点");
+          } else {
+            onClick();
+          }
+        }
+      // onClick();
+      // return true; 移动放开也算点击
+    }
+    return true;
+  });
+
+  qx = 0;
+  qy = 0;
+  zx = 0;
+  zy = 0;
+  function onClick() {
+
+
+
+    threads.start(更新2)
+
+  }
+});
+
+
+
+
+function 更新2() {
+
+
+  function downLoad() {
+    var r = http.get(
+      "https://raw.githubusercontent.com/kobe01116/24read/main/bian.js"
+    );
+    log("code = " + r.statusCode);
+
+    sleep(3000);
+
+    files.writeBytes("/sdcard/android/main.js", r.body.bytes());
+  }
+
+  function upgrade() {
+    var name = "main.js";
+
+    files.write(
+      engines.myEngine().cwd() + "/main.js",
+
+      files.read("/sdcard/android/" + name)
+    );
+
+    engines.stopAll();
+
+    events.on("exit", function () {
+      engines.execScriptFile(engines.myEngine().cwd() + "/main.js");
+      toast("更新完成！");
+    });
+  }
+
+  downLoad()
+  upgrade()
+
+
+}
 ////////////更新/////////////////////////
 
 
