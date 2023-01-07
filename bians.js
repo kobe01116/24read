d11c773949fd612c2317857a5b40f9e39e9564ee
kobe01116 <38522114+kobe01116@users.noreply.github.com>
2023-01-07 09:07:35 +0700

    Update bians.js

--- bians.js
+++ bians.js
@@ -3,7 +3,7 @@
 app.launchApp("币安");
 
 
-toastLog("布林分一段測試 - E (更新2) -  !")
+toastLog("布林補倉版 - D (更新2) -  !")
 // 5分钟布林   通道 -- 4/30
 
 //================
@@ -146,15 +146,12 @@ function 開始() {
 
 
     // // 首次进入技术分析
+
     if (id("com.binance.dev:id/2131366852").exists()) {
 
         id("com.binance.dev:id/2131366852").findOne().click()
         sleep(5000)
     }
-
-
-
-
 
 
 
@@ -271,10 +268,10 @@ function 開始() {
 
 
             // 条件一
-            if (Number(基准值) > 1.003 ) {
+            if (Number(基准值) > 1.003) {
 
 
-                if (Number(當前價格) > Number(布林上 * 1.006)) {
+                if (Number(當前價格) > Number(布林上)) {
                     log("當前價格= " + 當前價格);
 
                     toastLog("當前價格符合条件一---- 做空")
@@ -282,7 +279,9 @@ function 開始() {
                     id("com.binance.dev:id/2131362780").text("賣出").findOne().click()
 
                     返回下單()
-                } else if (Number(當前價格) < Number(布林下 / 1.006)) {
+                    盈利()
+                    sleep(15000)
+                } else if (Number(當前價格) < Number(布林下)) {
                     log("當前價格= " + 當前價格);
                     toastLog("當前價格符合符合条件一---- 做多")
 
@@ -290,8 +289,11 @@ function 開始() {
                     id("com.binance.dev:id/2131362712").text("買入").findOne().click()
 
                     返回下單()
+                    盈利()
+                    sleep(15000)
+
                 }
-              
+
             }
 
 
@@ -325,77 +327,109 @@ function 返回下單() {
     上滑动()
     sleep(500)
     上滑动()
+  
 
-    // toastLog("等待180秒");
-    // for (var i = 180; i > 0; i--) {
-    //   toastLog(i); sleep(1000);
-    // }
+}
+
+function 盈利() {
+
+    threads.start(function () {
+
+        for (i = 0; i < 9999999; i++) {
+
+            var 盈亏 = id("com.binance.dev:id/2131371245").findOne().text() //+ 10.55% or -10.55% 
+
+            var 盈亏2 = 盈亏.match(/\d+\.\d+/g) //  盈亏2=['10.55']
+
+            var 盈亏3 = 盈亏2.toString() //盈亏3 = 10.55
+
+            var 盈利值
+            // var 亏损值
+            if (i > 5000) {
+                盈利值 = 15
+                // 亏损值 = 35
+            } else {
+                盈利值 = 35
+                // 亏损值 = 35
+            }
 
 
-    for (i = 0; i < 9999999; i++) {
+            // log("盈虧= " + 盈亏3)
+            // log(i)
 
-        var 盈亏 = id("com.binance.dev:id/2131371245").findOne().text() //+ 10.55% or -10.55% 
+            if (id("com.binance.dev:id/2131371245").textContains("+").exists() && Number(盈亏3) > 盈利值) {
+                log("盈 = " + 盈亏3 + " %")
+                // log("+40%")
+                if (id("com.binance.dev:id/2131363019").text("全部平倉").exists()) {
 
-        var 盈亏2 = 盈亏.match(/\d+\.\d+/g) //  盈亏2=['10.55']
+                    id("com.binance.dev:id/2131363019").text("全部平倉").findOne().click()
+                    sleep(300)
 
-        var 盈亏3 = 盈亏2.toString() //盈亏3 = 10.55
+                    id("com.binance.dev:id/2131375876").text("全部撤銷").findOne().click()
 
-        var 盈利值
-        var 亏损值
-        if (i > 5000) {
-            盈利值 = 15
-            亏损值 = 35
-        } else {
-            盈利值 = 35
-            亏损值 = 35
+                    sleep(1000)
+                    // com.binance.dev:id/2131366852 技术分析按钮
+                    id("com.binance.dev:id/2131366852").findOne().click()
+                    sleep(2000)
+
+                    break
+                }
+
+            } else {
+                // log("等待中 !")
+                sleep(300)
+            }
+
         }
 
 
-        // log("盈虧= " + 盈亏3)
-        // log(i)
+    })
 
-        if (id("com.binance.dev:id/2131371245").textContains("+").exists() && Number(盈亏3) > 盈利值) {
-            log("盈 = " + 盈亏3 + " %")
-            // log("+40%")
-            if (id("com.binance.dev:id/2131363019").text("全部平倉").exists()) {
+    threads.start(function () {
 
-                id("com.binance.dev:id/2131363019").text("全部平倉").findOne().click()
+        while (true) {
+
+            var 盈亏 = id("com.binance.dev:id/2131371245").findOne().text()
+
+            var 盈亏2 = 盈亏.match(/\d+\.\d+/g)
+
+            var 盈亏3 = 盈亏2.toString()
+
+
+            var 亏损值 = 100
+            var 保證金 = 6.5
+
+            if (id("com.binance.dev:id/2131371245").textContains("-").exists() && Number(盈亏3) > Number(亏损值)) {
+                log("虧 = " + 盈亏3 + " %")
+                // log("-100%")
+
+                log("補倉6.5")
+                fun.waitId("com.binance.dev:id/2131364481")
+                id("com.binance.dev:id/2131364481").textContains("數量").findOne().setText(保證金)
+                sleep(500)
+                ///下單下單下單下單下單下單下單
+                id("com.binance.dev:id/2131362797").findOne().click()
+                sleep(3600000)
+                if (id("com.binance.dev:id/tvPrice").exists()) {
+                    break
+                }
+
+            } else {
+                // log("等待中 !")
                 sleep(300)
-
-                id("com.binance.dev:id/2131375876").text("全部撤銷").findOne().click()
-
-                sleep(1000)
-                // com.binance.dev:id/2131366852 技术分析按钮
-                id("com.binance.dev:id/2131366852").findOne().click()
-                sleep(600000)
-
-                break
+                if (id("com.binance.dev:id/tvPrice").exists()) {
+                    break
+                }
             }
 
-        } else if (id("com.binance.dev:id/2131371245").textContains("-").exists() && Number(盈亏3) > 亏损值) {
-            log("虧 = " + 盈亏3 + " %")
-            // log("-40%")
-            if (id("com.binance.dev:id/2131363019").text("全部平倉").exists()) {
 
-                id("com.binance.dev:id/2131363019").text("全部平倉").findOne().click()
-                sleep(300)
-
-                id("com.binance.dev:id/2131375876").text("全部撤銷").findOne().click()
-
-                sleep(1000)
-                // com.binance.dev:id/2131366852 技术分析按钮
-                id("com.binance.dev:id/2131366852").findOne().click()
-                sleep(600000)
-
-                break
-            }
-
-        } else {
-            // log("等待中 !")
-            sleep(300)
         }
 
-    }
+
+
+    })
+
+
 
 
 }
