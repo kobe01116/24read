c2446830efe92ee484feb931f588da1df976d54a
kobe01116 <38522114+kobe01116@users.noreply.github.com>
2023-02-04 09:27:28 +0700

    Update newbian.js

--- newbian.js
+++ newbian.js
@@ -3,7 +3,7 @@
 ui.layout(
     <vertical>
         <appbar>
-            <toolbar title="AJ 币安合约自动交易5.0 " />
+            <toolbar title="AJ 币安合约自动交易1.0 " />
             <tabs id="tabs" />
         </appbar>
         <card
@@ -839,7 +839,7 @@ function 開始() {
 
                 布林中 = res
 
-                // sleep(500)
+                sleep(500)
 
                 //  log("布林中= " + 布林中);
                 // sleep(3000)
@@ -871,15 +871,6 @@ function 開始() {
                 基准值 = Number(布林中) / Number(布林下)
                 布林上 = Number(布林中) * Number(基准值)
 
-                //布林中=(Number(布林上)+Number(布林下))/2
-
-                //toastLog( "布林上= "+布林上);
-                // log("布林下= " + 布林下);
-                // log("布林中= " + 布林中);
-                // log("基准值= " + 基准值);
-                //log("布林上= " + 布林上);
-
-                // 1秒更新一次
                 sleep(1000)
 
             }
@@ -899,7 +890,7 @@ function 開始() {
             // 找當前價格
             if ((布林中) > 0 && (布林下) > 0) {
 
-                toastLog(布林中+" , "+布林下+" , "+基准值+" , "+基數)
+                // toastLog(布林中+" , "+布林下+" , "+基准值+" , "+基數)
                 var 當前價格 = id("com.binance.dev:id/tvPrice").findOne().text()
 
 
